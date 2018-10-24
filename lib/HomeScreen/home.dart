000e09529e175aef2d2b8d7c0518d0aaba738da1
Jack hosking <jackhosking2@outlook.com>
2018-10-24 22:42:22 +1300

    await causing issues, refactor to pass from main with root

--- lib/HomeScreen/home.dart
+++ lib/HomeScreen/home.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'dart:async';
-
+import 'package:bitbuddy/main.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,66 +7,68 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  List currency ;
-
-  @override
-  void initState() async{
-    super.initState();
-    currency = await getCryptoCurrency();
-  }
-
-  Future<List> getCryptoCurrency() async{
-      String coinMarketCapAPI = "https://api.coinmarketcap.com/v2/ticker/?limit=10";
-      http.Response response = await http.get(coinMarketCapAPI);
-      return json.decode(response.body);
-  }
+  List currency;
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Bitbuddy")
-      ),
+      appBar: new AppBar(title: new Text("Bitbuddy")),
       body: _cryptoHolder(),
     );
   }
 
-  Widget _cryptoHolder()
-  {
+  Widget _cryptoHolder() {
     return new Container(
-      child: new Flexible(
-        child: new ListView.builder(
-            itemCount: currency.length,
-            padding: const EdgeInsets.all(5.0),
-            itemBuilder: (BuildContext context, int index) {
-                final Map currencyMap = currency[index];
+        child: new Flexible(
+            child: new ListView.builder(
+      itemCount: currency.length,
+      padding: const EdgeInsets.all(5.0),
+      itemBuilder: (BuildContext context, int index) {
+        final Map currencyMap = currency[index];
 
-                return _buildListItem(currencyMap);
-            },
-        )
-      )
-    );
+        return _buildListItem(currencyMap);
+      },
+    )));
   }
 
-  Widget _buildListItem(Map currencyMap){
+  Widget _buildListItem(Map currencyMap) {
     return new Container(
       child: new Card(
         child: new Column(
-        mainAxisSize: MainAxisSize.min,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             new ListTile(
               leading: new CircleAvatar(
                 child: new Text(currencyMap['name'][0]),
               ),
               title: new Text(currencyMap['name'],
-              style: new TextStyle(fontWeight: FontWeight.bold)),
-
-              ),
+                  style: new TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: _getPriceToSubtitle(
+                  currencyMap['price_usd'], currencyMap['percent_change_1h']),
+              isThreeLine: true,
             )
           ],
         ),
       ),
     );
+  }
+
+  Widget _getPriceToSubtitle(String price, String change) {
+    TextSpan priceWidget = new TextSpan(
+        text: "\$$price\n",
+        style: new TextStyle(color: Color.fromRGBO(115, 222, 255, 1.0)));
+    String changeWidget = "60 minute change: $change%";
+    TextSpan changeTextWidget;
+
+    if (double.parse(change) >= 0) {
+      changeTextWidget = new TextSpan(
+          text: changeWidget, style: new TextStyle(color: Colors.green));
+    } else {
+      changeTextWidget = new TextSpan(
+          text: changeWidget, style: new TextStyle(color: Colors.red));
+    }
+
+    return new RichText(
+        text: new TextSpan(children: [priceWidget, changeTextWidget]));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:bitbuddy/HomeScreen/home.dart';
 import 'package:bitbuddy/splash_screen.dart';
 import 'package:bitbuddy/utils/routers.dart';
-
 
 var routes = <String, WidgetBuilder>{
   Routers.home: (BuildContext context) => Home(),
 };
 
-void main() => runApp(new MaterialApp(
-  theme: ThemeData(primaryColor:  Colors.white),
-  title: 'Bitbuddy',
-  home: new SplashScreen(),
-  routes: routes
-));
+void main() async {
+  List currency = await getCryptoCurrency();
+  print(currency);
+  runApp(new Bitbuddy(currency));
+}
+
+class Bitbuddy extends StatelessWidget {
+  final List _currency;
+  Bitbuddy(this._currency);
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        theme: ThemeData(primaryColor: Colors.white),
+        title: 'Bitbuddy',
+        home: new SplashScreen(),
+        routes: routes);
+  }
+}
+
+Future<List> getCryptoCurrency() async {
+  String coinMarketCapAPI = "https://api.coinmarketcap.com/v2/ticker/?limit=10";
+  http.Response response = await http.get(coinMarketCapAPI);
+  return json.decode(response.body);
+}
