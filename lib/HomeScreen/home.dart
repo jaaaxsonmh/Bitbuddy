8e0e1c7a14319799999e13719085302add2bfa37
Jack hosking <jackhosking2@outlook.com>
2018-10-25 01:07:57 +1300

    get data to display.
    Error for null is within the next object of json

--- lib/HomeScreen/home.dart
+++ lib/HomeScreen/home.dart
@@ -17,9 +17,11 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     print(widget.currency);
+    print(widget.currency.length);
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Bitbuddy"),
-      centerTitle: true,
+      appBar: new AppBar(
+        title: new Text("Bitbuddy"),
+        centerTitle: true,
       ),
       body: _cryptoHolder(),
     );
@@ -31,7 +33,7 @@ class _HomeState extends State<Home> {
       children: <Widget>[
         new Flexible(
             child: new ListView.builder(
-          itemCount: 5,
+          itemCount: widget.currency.length,
           padding: const EdgeInsets.all(5.0),
           itemBuilder: (BuildContext context, int index) {
             final Map currencyMap = widget.currency[index];
@@ -44,41 +46,48 @@ class _HomeState extends State<Home> {
   }
 
   Widget _buildListItem(BuildContext context, Map currencyList) {
+
+    int rank = currencyList['rank'];
+    double supply = currencyList['total_supply'] - currencyList['circulating_supply'];
+
     return new Container(
       child: new Card(
         child: new Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             new ListTile(
-              leading: new CircleAvatar(
-                child: new Text(currencyList['name'][0]),
-              ),
-              title: new Text(currencyList['name'],
-                  style: new TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: _getPriceToSubtitle(
-                  currencyList['price_usd'], currencyList['percent_change_1h']),
+                leading: new CircleAvatar(
+                  child: new Text(currencyList['name'][0]),
+                ),
+                title: new Text(currencyList['name'],
+                    style: new TextStyle(fontWeight: FontWeight.bold)),
+                subtitle: new Text("Rank: $rank \nCoins not in circulation: $supply")
+
+                /*_getPriceToSubtitle(
+                  currencyList['rank'], currencyList['total_supply']),
               isThreeLine: true,
-            )
+              */
+                )
           ],
         ),
       ),
     );
   }
 
-  Widget _getPriceToSubtitle(String price, String change) {
+  Widget _getPriceToSubtitle(int price, int change) {
     TextSpan priceWidget = new TextSpan(
-        text: "\$$price\n",
+        text: "Rank: price\n",
         style: new TextStyle(color: Color.fromRGBO(115, 222, 255, 1.0)));
-    String changeWidget = "60 minute change: $change%";
+    String changeWidget = "total supply: $change";
     TextSpan changeTextWidget;
 
-    if (double.parse(change) >= 0) {
+    /* if (double.parse(change) >= 0) {
       changeTextWidget = new TextSpan(
           text: changeWidget, style: new TextStyle(color: Colors.green));
     } else {
       changeTextWidget = new TextSpan(
           text: changeWidget, style: new TextStyle(color: Colors.red));
-    }
+    }*/
 
     return new RichText(
         text: new TextSpan(children: [priceWidget, changeTextWidget]));
