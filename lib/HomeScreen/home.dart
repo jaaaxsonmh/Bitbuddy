96724e5bf4e547e8435def0d3def86696af08e47
Jack hosking <jackhosking2@outlook.com>
2018-10-25 12:17:53 +1300

    Change icon

--- lib/HomeScreen/home.dart
+++ lib/HomeScreen/home.dart
@@ -60,7 +60,7 @@ class _HomeState extends State<Home> {
                 leading: new CircleAvatar(
                   radius: 30.0,
                   backgroundColor: Color.fromRGBO(115, 222, 255, 1.0),
-                  child: Image.network("https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@0b37b127fbec5091a47921deeba2dce2c1f8eb16/32@2x/color/"+currencyList['symbol'].toLowerCase()+"@2x.png")/*new Text(currencyList['symbol'], style: new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic, fontSize: 18.0),),*/
+                  child: Image.network("https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@0b37b127fbec5091a47921deeba2dce2c1f8eb16/128/color/"+currencyList['symbol'].toLowerCase()+".png")/*new Text(currencyList['symbol'], style: new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic, fontSize: 18.0),),*/
                 ),
                 title: new Text(currencyList['name'],
                     style: new TextStyle(color:  Colors.black, fontSize: 24.0)),
