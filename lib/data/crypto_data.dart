5c7fa4e2463a606662383f8a4df21a6ea1f1f857
Jack hosking <jackhosking2@outlook.com>
2018-10-26 22:05:27 +1300

    Small fix,
    API data fields

--- lib/data/crypto_data.dart
+++ lib/data/crypto_data.dart
@@ -1,28 +1,48 @@
 import 'dart:async';
 
 class CryptoData {
+  int id;
   String name;
   String symbol;
+  String website_slug;
+  int rank;
+  double circulating_supply;
+  double total_supply;
+  double max_supply;
   double price;
+  double volume_24hour;
+  double market_cap;
   double percent_change_1h;
   double percent_change_12h;
   double percent_change_24h;
 
   CryptoData(
-      {this.name,
-      this.price,
+      {this.id,
+      this.name,
       this.symbol,
+      this.website_slug,
+      this.rank,
+      this.circulating_supply,
+      this.total_supply,
+      this.max_supply,
+      this.price,
+      this.volume_24hour,
+      this.market_cap,
       this.percent_change_1h,
       this.percent_change_12h,
       this.percent_change_24h});
 
   CryptoData.fromMap(Map<String, dynamic> map)
-      : name = map['name'],
+      : id = map['id'],
+        name = map['name'],
+        symbol = map['symbol'],
+        website_slug= map['website_slug'],
+        rank = map['rank'],
+        circulating_supply = map['circulating_supply'],
         price = map['quotes']['USD']['price'],
         percent_change_1h = map['quotes']['USD']['percent_change_1h'],
         percent_change_12h = map['quotes']['USD']['percent_change_12h'],
-        percent_change_24h = map['quotes']['USD']['percent_change_24h'],
-        symbol = map['symbol'];
+        percent_change_24h = map['quotes']['USD']['percent_change_24h'];
 }
 
 abstract class CryptoStore {
@@ -36,6 +56,6 @@ class GetDataException implements Exception {
 
   String toString() {
     if (_exception == null) return "Unknown";
-      return " Exception $_exception";
+    return " Exception $_exception";
   }
 }
