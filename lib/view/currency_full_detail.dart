c9498a953205084790f7952f3abb864d542a5bcb
Jack hosking <jackhosking2@outlook.com>
2018-11-14 01:36:01 +1300

    converter of currency

--- lib/view/currency_full_detail.dart
+++ lib/view/currency_full_detail.dart
@@ -1,19 +1,21 @@
-
 import 'package:flutter/material.dart';
 import 'package:bitbuddy/data/crypto_data.dart';
 import 'package:bitbuddy/widgets/price_formated.dart';
 
 class CurrencyFullDetail extends StatefulWidget {
   final CryptoData currencyList;
+
   CurrencyFullDetail(this.currencyList);
 
-@override
-CurrencyFullDetailState createState() => new CurrencyFullDetailState();
+  @override
+  CurrencyFullDetailState createState() =>
+      new CurrencyFullDetailState(currencyList);
 }
 
 class CurrencyFullDetailState extends State<CurrencyFullDetail> {
+  final CryptoData currencyList;
 
-  CryptoData currencyList;
+  CurrencyFullDetailState(this.currencyList);
 
   final TextEditingController _controllerCrypto = new TextEditingController();
   final TextEditingController _controllerFiat = new TextEditingController();
@@ -32,20 +34,46 @@ class CurrencyFullDetailState extends State<CurrencyFullDetail> {
       appBar: new AppBar(
         leading: Image.asset("images/bitbuddylogo.jpg"),
         backgroundColor: Color.fromRGBO(115, 222, 255, 1.0),
-        title: new Text("Currency Summary",
+        title: new Text(currencyList.name + " Summary",
             style: new TextStyle(color: Colors.white, fontSize: 28.0)),
         centerTitle: true,
       ),
       body: new Container(
-        child: _buildHeaderCard(currencyList) +  _buildConverterCalculator(currencyList),
+        decoration: new BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.rectangle,
+          borderRadius: new BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          children: <Widget>[
+            new Padding(padding: EdgeInsets.fromLTRB(0.0, 6.0, 0.0, 0.0),
+              child: new Text("Currency Ticker",
+                  style: new TextStyle(
+                    fontSize: 32.0,
+                    color: Color.fromRGBO(115, 222, 255, 1.0),
+
+                  )),
+            ),
+            _buildHeaderCard(currencyList),
+            new Padding(padding: EdgeInsets.fromLTRB(0.0, 6.0, 0.0, 0.0),
+              child: new Text("Currency Converter",
+                  style: new TextStyle(
+                    fontSize: 32.0,
+                    color: Color.fromRGBO(115, 222, 255, 1.0),
+
+                  )),
+            ),
+            _buildConverterCalculator(currencyList),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildHeaderCard(CryptoData currencyList) {
     return new Container(
-      child: new Card(
-        child: new GestureDetector(
+      padding: EdgeInsets.only(top: 16.0),
+      child: new GestureDetector(
           child: new Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
@@ -56,11 +84,12 @@ class CurrencyFullDetailState extends State<CurrencyFullDetail> {
                     child: Image.network(
                         "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@0b37b127fbec5091a47921deeba2dce2c1f8eb16/128/color/" +
                             currencyList.symbol.toLowerCase() +
-                            ".png") /*new Text(currencyList['symbol'], style: new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic, fontSize: 18.0),),*/
-                ),
+                            ".png")),
                 title: new Text(currencyList.name,
                     style: new TextStyle(color: Colors.black, fontSize: 24.0)),
-                subtitle:  new PriceFormatted(price: currencyList.price, change: currencyList.percentChange1h),
+                subtitle: new PriceFormatted(
+                    price: currencyList.price,
+                    change: currencyList.percentChange1h),
                 isThreeLine: false,
                 trailing: new Text(
                   currencyList.symbol,
@@ -73,61 +102,91 @@ class CurrencyFullDetailState extends State<CurrencyFullDetail> {
             ],
           ),
         ),
-      ),
     );
   }
 
   Widget _buildConverterCalculator(CryptoData currencyList) {
-    new Container(
-        margin: new EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+    return new Container(
 
-        decoration: new BoxDecoration(
-          color: new Color(0xFFFFFFFF),
-          shape: BoxShape.rectangle,
-          borderRadius: new BorderRadius.circular(10.0),
-          boxShadow: <BoxShadow>[
-            new BoxShadow(
-              color: new Color(0xFFee0979),
-              blurRadius: 10.0,
-              offset: new Offset(0.0, 10.0),
-            ),
-          ],
-        ),
-
-        child: new Column(children: <Widget>[
-        new Stack(
-        alignment: const Alignment(1.0, 0.5),
+      margin: new EdgeInsets.all(5.0),
+      child: new Column(
         children: <Widget>[
-          new TextField(
-            controller: _controllerCrypto,
-            maxLines: 1,
-            keyboardType: TextInputType.number,
-            onChanged: (newValue) {
-              setState(() {
-                cryptoString = newValue.trim();
-                if(fiatString.length==0){
-                  _controllerFiat.text = " ";
-                }
-                _controllerFiat.text = (double.parse(cryptoString)*double.parse(widget.crypto.priceUsd)).toString();
-              });
-            },
-            decoration: new InputDecoration(
-              border: InputBorder.none,
-              contentPadding: new EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 10.0),
-              hintStyle: Theme.TextStyles.commonTextStyle,
-              labelStyle: Theme.TextStyles.commonTextStyle,
-              labelText: currencyList.symbol,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          new FlatButton(
-              onPressed: () {
-                _controllerCrypto.clear();
-                _controllerFiat.clear();
+          new Stack(alignment: const Alignment(1.0, 0.5), children: <Widget>[
+            new TextField(
+              controller: _controllerCrypto,
+              maxLines: 1,
+              keyboardType: TextInputType.number,
+              onChanged: (newValue) {
+                setState(() {
+                  cryptoString = newValue.trim();
+                  if (fiatString.length == 0) {
+                    _controllerFiat.text = "Nothing here yet";
+                  }
+                  _controllerFiat.text =
+                      "\$" + (double.parse(cryptoString) * (currencyList.price))
+                          .toString();
+                });
               },
-              child: new Icon(Icons.clear, color: Colors.black,)
-          ),
-        ]
-    ),
+              decoration: new InputDecoration(
+                border: InputBorder.none,
+                contentPadding: new EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 10.0),
+                labelText: currencyList.symbol,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            clearField(),
+          ]),
+          new Divider(color: Color.fromRGBO(115, 222, 255, 1.0)),
+          new Stack(alignment: const Alignment(1.0, 0.5), children: <Widget>[
+            new TextField(
+              controller: _controllerFiat,
+              maxLines: 1,
+              keyboardType: TextInputType.number,
+              onChanged: (newValue) {
+                setState(() {
+                  fiatString = newValue.trim();
+                  if (cryptoString.length == 0) {
+                    _controllerCrypto.text = "Nothing here yet";
+                  }
+                  if ((double.parse(fiatString) /
+                      (currencyList.price))
+                      .toString()
+                      .length >=
+                      10) {
+                    _controllerCrypto.text = (double.parse(fiatString) /
+                        (currencyList.price))
+                        .toString()
+                        .substring(0, 8);
+                  } else {
+                    _controllerCrypto.text = (double.parse(fiatString) /
+                        (currencyList.price))
+                        .toString();
+                  }
+                });
+              },
+              decoration: new InputDecoration(
+                border: InputBorder.none,
+                contentPadding: new EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 10.0),
+                labelText: "USD",
+              ),
+              textAlign: TextAlign.center,
+            ),
+            clearField()
+          ]),
+        ],
+      ),
+    );
+  }
+
+  FlatButton clearField() {
+    return new FlatButton(
+        onPressed: () {
+          _controllerCrypto.clear();
+          _controllerFiat.clear();
+        },
+        child: new Icon(
+          Icons.clear,
+          color: Colors.black,
+        ));
   }
 }
